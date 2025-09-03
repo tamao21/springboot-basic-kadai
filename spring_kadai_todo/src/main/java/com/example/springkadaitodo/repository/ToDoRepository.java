06e3c2c2ca3bba269429ae4dc8cd636e376b5e49
tamao21 <tt21komasan@icloud.com>
2025-09-03 18:28:34 +0900

    Add files via upload

---
+++ spring_kadai_todo/src/main/java/com/example/springkadaitodo/repository/ToDoRepository.java
@@ -0,0 +1,13 @@
+package com.example.springkadaitodo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springkadaitodo.entity.ToDo;
+
+public interface ToDoRepository extends JpaRepository<ToDo, Integer> {
+
+	// ユーザー名で完全一致検索
+	List<ToDo> findAll();
+}
